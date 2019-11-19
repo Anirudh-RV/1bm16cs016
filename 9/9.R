9ea525e39cde7d4dbb227cf7392b43993a73917b
Anirudh-RV <45145236+Anirudh-RV@users.noreply.github.com>
2019-11-19 13:51:20 +0530

    Add files via upload

---
+++ 9/9.R
@@ -0,0 +1,15 @@
+library("rpart")
+library("rpart.plot")
+
+p <- read.csv("DT.csv")
+p2 <- data.frame(p)
+p2
+
+fit<-rpart(play ~ outlook + Temperature  +Humidity +Wind,method="class",data=p2,control=rpart.control(minsplit=1),parms=list(split='information'))
+fit
+str(fit)
+rpart.plot(fit,type=4,extra=1)
+rpart.plot(fit,type=4,extra=2,clip.right.labs=FALSE,varlen=0,faclen=0)
+
+rpart.plot
+print('1bm16cs016')
